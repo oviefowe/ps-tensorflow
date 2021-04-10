6cf1ed6d50f5145fc6a6e6a31ed03aa2519a1998
Emmanuel Doro <y837577@nazmacv544md6p.fios-router.home>
2021-04-10 00:06:02 -0400

    adding graphs and functions

--- basic_intros/basic.py
+++ basic_intros/basic.py
@@ -1,6 +1,21 @@
 import os
-
+import cProfile
 import tensorflow as tf
 
-import cProfile
+def fizzbuzz(max_num):
+  counter = tf.constant(0)
+  max_num = tf.convert_to_tensor(max_num)
+  for num in range(1, max_num.numpy()+1):
+    num = tf.constant(num)
+    if int(num % 3) == 0 and int(num % 5) == 0:
+      print('FizzBuzz')
+    elif int(num % 3) == 0:
+      print('Fizz')
+    elif int(num % 5) == 0:
+      print('Buzz')
+    else:
+      print(num.numpy())
+    counter += 1
 
+if __name__ == "__main__":
+    fizzbuzz(30)
